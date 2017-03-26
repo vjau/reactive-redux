fb934776885812688a392263a58ae8724a71dd06
Vincent Jaubert <contact@vjaubert.com>
2017-03-26 21:46:21 +0200

    Added ability to watch the result of a selector

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
-import {Tracker} from "meteor/tracker";
 import {get as pathGet} from 'object-path';
 import initSubscriber from "redux-subscriber";
-import {subscribe} from "redux-subscriber";
+import {Tracker} from "meteor/tracker";
+
+const watchSelector = (selector, Store, cb)=>{
+  let prevComputedState = selector(Store.getState());
+  const compare = (newState)=>{
+    let newComputedState = selector(newState);
+    if (prevComputedState !== newComputedState){
+      prevComputedState = newComputedState;
+      cb();
+    }
+  };
+  return Store.subscribe(()=>(compare(Store.getState())));
+};
 
 
 let subscriber;
 
-const factory = (path, Store)=>{
-  if (typeof path !== "string"){
-    throw new Error("factory path should be a string");
+const factory = (pathOrSelector, Store)=>{
+  if (typeof pathOrSelector !== "string" && typeof pathOrSelector !== "function"){
+    throw new Error("factory pathOrSelector should be a string or a function");
   }
   if (!Store || typeof Store.getState !== "function"){
     throw new Error("factory should be provided with a Store");
@@ -18,17 +29,34 @@ const factory = (path, Store)=>{
     subscriber = initSubscriber(Store);
   }
   const dep = new Tracker.Dependency();
-  const unsubscribe = subscriber(path, ()=>(dep.changed()));
-  return {
-    get(){
-      dep.depend();
-      return pathGet(Store.getState(), path);
-    },
-    cancel(){
-      unsubscribe();
+  if (typeof pathOrSelector === "string"){
+    const unsubscribe = subscriber(pathOrSelector, ()=>(dep.changed()));
+    return {
+      get(){
+        dep.depend();
+        return pathGet(Store.getState(), pathOrSelector);
+      },
+      cancel(){
+        unsubscribe();
+      }
     }
   }
+  if (typeof pathOrSelector === "function"){
+    const unsubscribe = watchSelector(pathOrSelector, Store, ()=>(dep.changed()));
+    return {
+      get(){
+        dep.depend();
+        return pathOrSelector(Store.getState());
+      },
+      cancel(){
+        unsubscribe();
+      }
+    }
+  }
+
 };
+
+
 
 
 export default factory;
